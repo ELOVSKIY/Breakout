f773e16b7206f6bd8711351aed27f0f614b71277
Stas <stas.elovsky@gmail.com>
2020-10-22 18:31:20 +0300

    add main

--- main.cpp
+++ main.cpp
@@ -3,18 +3,17 @@
 #endif
 
 #include <windows.h>
-#include <windowsx.h>
 #include <new>
 #include <iostream>
-#include <fstream>
 #include <string>
 #include <vector>
-#include <sstream>
-#include <algorithm>
 #include <iterator>
+#include "game/Game.h"
+
 using namespace std;
 
 struct StateInfo {
+    Game *game;
     int clientWidth, clientHeight;
     int colCount, rowCount;
     vector<vector<string>> content;
@@ -23,11 +22,11 @@ struct StateInfo {
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
-{
-    const wchar_t CLASS_NAME[] = L"Sample Window Class";
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow) {
 
-    WNDCLASS wc = { };
+    const wchar_t CLASS_NAME[] = L"Main Break Out Class";
+
+    WNDCLASS wc = {};
 
     wc.lpfnWndProc = WindowProc;
     wc.hInstance = hInstance;
@@ -35,10 +34,9 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow
 
     RegisterClass(&wc);
 
-    StateInfo* pState = new (std::nothrow) StateInfo;
+    StateInfo *pState = new(std::nothrow) StateInfo;
 
-    if (pState == NULL)
-    {
+    if (pState == NULL) {
         return 0;
     }
 
@@ -46,9 +44,9 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow
             0,
             CLASS_NAME,
             L"Breakout",
-            WS_OVERLAPPEDWINDOW,
+            WS_OVERLAPPED,
 
-            CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+            CW_USEDEFAULT, CW_USEDEFAULT, 1920, 1080,
 
             NULL,
             NULL,
@@ -56,17 +54,15 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow
             pState
     );
 
-    if (hwnd == NULL)
-    {
+    if (hwnd == NULL) {
         return 1;
     }
 
-    ShowWindow(hwnd, nCmdShow);
+    ShowWindow(hwnd, SW_SHOWMAXIMIZED);
 
 
-    MSG msg = { };
-    while (GetMessage(&msg, NULL, 0, 0))
-    {
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0)) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
@@ -74,11 +70,10 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow
     return 0;
 }
 
-inline StateInfo* GetAppState(
-        HWND hwnd)
-{
+inline StateInfo *GetAppState(
+        HWND hwnd) {
     LONG_PTR ptr = GetWindowLongPtr(hwnd, GWLP_USERDATA);
-    StateInfo* pState = reinterpret_cast<StateInfo*>(ptr);
+    StateInfo *pState = reinterpret_cast<StateInfo *>(ptr);
     return pState;
 }
 
@@ -86,14 +81,12 @@ LRESULT CALLBACK WindowProc(
         HWND hwnd,
         UINT uMsg,
         WPARAM wParam,
-        LPARAM lParam)
-{
-    StateInfo* pState;
-    if (uMsg == WM_CREATE)
-    {
-        CREATESTRUCT* pCreate = reinterpret_cast<CREATESTRUCT*>(lParam);
-        pState = reinterpret_cast<StateInfo*>(pCreate->lpCreateParams);
-        SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)pState);
+        LPARAM lParam) {
+    StateInfo *pState;
+    if (uMsg == WM_CREATE) {
+        CREATESTRUCT *pCreate = reinterpret_cast<CREATESTRUCT *>(lParam);
+        pState = reinterpret_cast<StateInfo *>(pCreate->lpCreateParams);
+        SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR) pState);
 
         RECT winRect;
 
@@ -102,24 +95,26 @@ LRESULT CALLBACK WindowProc(
             pState->clientHeight = winRect.bottom - winRect.top;
         }
 
-    }
-    else
-    {
+    } else {
         pState = GetAppState(hwnd);
     }
-
-    switch (uMsg)
-    {
+//    pState->game->GetInput(uMsg, wParam, lParam);
+    switch (uMsg) {
         case WM_DESTROY:
             PostQuitMessage(0);
             return 0;
 
-        case WM_PAINT:
-        {
+        case WM_PAINT: {
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hwnd, &ps);
 
-            FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+            pState->game->Draw(hdc);
+//            FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+//            HDC memHdc = CreateCompatibleDC(hdc);
+//            HBRUSH hbrush = CreateSolidBrush(RGB(200,200,200));
+//            SelectObject(hdc,hbrush);
+//            Rectangle(hdc, 200, 200, 500, 500);
+//            DeleteObject(hbrush);
 
             EndPaint(hwnd, &ps);
         }
@@ -128,6 +123,7 @@ LRESULT CALLBACK WindowProc(
         case WM_SIZE:
             pState->clientWidth = LOWORD(lParam);
             pState->clientHeight = HIWORD(lParam);
+            pState->game = new Game(pState->clientWidth, pState->clientHeight);
             return 0;
 
     }
